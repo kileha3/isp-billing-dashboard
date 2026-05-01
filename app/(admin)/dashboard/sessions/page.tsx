a11e53eef354ad66ade248a7a845c770036c3e3b
Lukundo Kileha <lkileha@gmail.com>
2026-05-01 12:08:14 +0300

    Pushing for production - WIP

--- app/(admin)/dashboard/sessions/page.tsx
+++ app/(admin)/dashboard/sessions/page.tsx
@@ -354,7 +354,7 @@ export default function SessionsPage() {
       <Dialog open={historyDialog.isOpen} onOpenChange={(open) => {
         if (!open) setHistoryDialog({ isOpen: false, loading: false, sessions: [], currentSession: null });
       }}>
-        <DialogContent className={`!max-w-[${historyDialog.loading ? 30:80}vw] overflow-hidden flex flex-col`}>
+        <DialogContent className={`!w-[${historyDialog.loading ? 30:80}vw] !max-w-[${historyDialog.loading ? 30:80}vw] overflow-hidden flex flex-col`}>
           <DialogHeader>
             <DialogTitle>
               Session History
